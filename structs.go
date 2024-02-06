229392260d4c2814dd99cf5a852c82e16dfa1c67
kelyan.danis@ynov.com <kelyan.danis@ynov.com>
2024-02-06 08:49:23 +0100

    ⚡️ i implement the monster structure ⚡️

---
+++ structs.go
@@ -0,0 +1,40 @@
+package Monster
+
+type monster struct {
+	Name				string
+	KEY					int
+	ID					int
+	MonsterType			string
+	Size				string
+	Alignment			string
+	Caract				map[string]int
+	CaractMod			map[string]int
+	Mastery				int
+	AC					string
+	LP					string
+	Resistance			[]string
+	Vulnerability		[]string
+	Immunity			[]string
+	AttacBonnus			int
+	DD					int
+	Speed				map[string]int
+	SaveRoll			map[string]int
+	
+	StateImmunity		[]string
+	
+	Sense				[]string
+	Languages			[]string
+
+	Attaques			[]string
+	SpecialDeals		[]string
+	Action				[]string
+	Reaction			[]string
+}
+
+type groupMonstre struct {
+
+}
+
+type table_rencontre struct {
+
+}
