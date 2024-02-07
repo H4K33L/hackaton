ccf82db984c458af1d5d812e4ae1ad4b4bdc8b94
kelyan.danis@ynov.com <kelyan.danis@ynov.com>
2024-02-07 13:15:21 +0100

    ✨ I add Enconter Table generation ✨

--- testMonster/main.go
+++ testMonster/main.go
@@ -7,6 +7,15 @@ import (
 )
 
 func main() {
-	monstre1 := Monster.GenerateMonster("bête",3)
-	fmt.Println(monstre1)
+	//monstre1 := Monster.GenerateMonster("bête",3)
+	//fmt.Println(monstre1)
+	//bandOfMonster := Monster.GenerateGroup("bête",3200)
+	/*for i := range bandOfMonster.Monsters {
+		fmt.Println(bandOfMonster.Monsters[i].ID)
+	}*/
+	encounter := Monster.GenerateEncounter("bête",3) 
+	for i := range encounter.Encounter {
+		fmt.Println(encounter.Encounter[i])
+		fmt.Println()
+	}
 }
