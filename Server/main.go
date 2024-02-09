fae6117925048693e0b07ece3c03d07f50c5374e
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2024-02-09 10:41:26 +0100

    add comment on the main.go

--- Server/main.go
+++ Server/main.go
@@ -39,6 +39,11 @@ type groupMonster struct {
 
 
 func main() {
+	/*
+	The main func launch the server on the port 8080 and handle the different request.
+	---------------------------------------------------------
+	The func dosen't have error case.
+	*/
 	blue := "\033[1;34m"
 	green := "\033[1;32m"
 	red := "\033[1;31m"
@@ -58,24 +63,40 @@ func main() {
 }
 
 func generateJSON(w http.ResponseWriter, r *http.Request) {
-	number, err := strconv.Atoi(r.URL.Query().Get("number"))
+	/*
+	The generateJSON func add the monster to the JSON file.
+	---------------------------------------------------------
+	error case:
+		- if the number is not a number
+		- if the ID is not a number
+	*/
+	number, err := strconv.Atoi(r.URL.Query().Get("number")) // Get the number of monster to generate from the URL
 	if err != nil {
 		http.Error(w, "Erreur lors de la lecture du nombre", http.StatusBadRequest)
 		return
 	}
-	monsterType := r.URL.Query().Get("type")
-	ID, err := strconv.Atoi(r.URL.Query().Get("ID"))
+	monsterType := r.URL.Query().Get("type") // Get the type of monster to generate from the URL
+	ID, err := strconv.Atoi(r.URL.Query().Get("ID")) // Get the ID of the monster to generate from the URL
 	if err != nil {
 		http.Error(w, "Erreur lors de la lecture de l'ID", http.StatusBadRequest)
 		return
 	}
-	Monster.Generate(number, monsterType, ID)
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	Monster.Generate(number, monsterType, ID) // Generate the monsters
+	http.Redirect(w, r, "/", http.StatusSeeOther) // Redirect to the main page
 
-	handler(w, r)
+	handler(w, r) // Render the HTML file
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	/*
+	The handler func handle the different request.
+	---------------------------------------------------------
+	error case:
+		- if the file JSON is not found
+		- if the JSON file is not valid
+		- if the HTML file is not found
+		- if the HTML file is not valid
+	*/
 	filePath := "../Data/monster.json"
 	body, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -90,10 +111,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
     return
 	}
 
-	renderHTML(w, response.Monsters, "index.html")
+	renderHTML(w, response.Monsters, "index.html") // Render the HTML file
 }
 
 func renderHTML(w http.ResponseWriter, data interface{}, templateFile string) {
+	/*
+	The renderHTML func render the HTML file with the data.
+	---------------------------------------------------------
+	error case:
+		- if the HTML file is not found
+		- if the HTML file is not valid
+	*/
 	tmpl, err := template.ParseFiles(templateFile)
 	if err != nil {
 		http.Error(w, "Erreur lors du chargement du modèle HTML", http.StatusInternalServerError)
@@ -108,7 +136,14 @@ func renderHTML(w http.ResponseWriter, data interface{}, templateFile string) {
 }
 
 func deleteMonster(w http.ResponseWriter, r *http.Request) {
-    name := r.URL.Query().Get("name")
+	/*
+	The deleteMonster func delete the monster from the JSON file.
+	---------------------------------------------------------
+	error case:
+		- if the file JSON is not found
+		- if the JSON file is not valid
+	*/
+    name := r.URL.Query().Get("name") // Get the name of the monster to delete from the URL
     filePath := "../Data/monster.json"
     body, err := ioutil.ReadFile(filePath)
     if err != nil {
@@ -123,27 +158,27 @@ func deleteMonster(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    newMonsters := make([]monster, 0)
-    for _, monster := range response.Monsters {
-        if monster.Name != name {
-            newMonsters = append(newMonsters, monster)
+    newMonsters := make([]monster, 0) // Create a new slice of monster
+    for _, monster := range response.Monsters { // Add all the monsters except the one to delete
+        if monster.Name != name { // If the monster is not the one to delete
+            newMonsters = append(newMonsters, monster) // Add the monster to the new slice
         }
     }
-    response.Monsters = newMonsters
+    response.Monsters = newMonsters // Replace the old slice with the new one
 
-    newData, err := json.Marshal(response)
+    newData, err := json.Marshal(response) // Convert the new slice to JSON
     if err != nil {
         http.Error(w, "Erreur lors de la conversion en JSON", http.StatusInternalServerError)
         return
     }
 
-    err = ioutil.WriteFile(filePath, newData, 0644)
+    err = ioutil.WriteFile(filePath, newData, 0644) // Write the new JSON file
     if err != nil {
         http.Error(w, "Erreur lors de l'écriture dans le fichier JSON", http.StatusInternalServerError)
         return
     }
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther) // Redirect to the main page
 
-	handler(w, r)
+	handler(w, r) // Render the HTML file
 }
